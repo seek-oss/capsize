201a991eeb4987128ec5671a24b7514161c28084
Matt Jones <mattjones701@gmail.com>
2020-07-09 08:46:26 +1000

    Fix gatsby build for site

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -5,7 +5,7 @@
  */
 
 // You can delete this file if you're not using it
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
   if (stage === 'develop' || stage === 'build-javascript') {
     actions.setWebpackConfig({
       node: {
@@ -22,6 +22,24 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
         ],
       },
     });
+  }
+
+  if (stage === 'build-html') {
+    const config = getConfig();
+
+    const [gatsbyExternals] = config.externals;
+
+    function externals(context, request, callback) {
+      if (request.indexOf('fontkit') > -1) {
+        return callback(null, `commonjs ${request}`);
+      }
+
+      return gatsbyExternals(context, request, callback);
+    }
+
+    config.externals = [externals];
+
+    actions.replaceWebpackConfig(config);
   }
 };
 
--- site/gatsby-ssr.js
+++ site/gatsby-ssr.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react';
+import {
+  ThemeProvider,
+  ColorModeProvider,
+  CSSReset,
+  theme,
+} from '@chakra-ui/core';
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider value="light">
+      <CSSReset />
+      {element}
+    </ColorModeProvider>
+  </ThemeProvider>
+);

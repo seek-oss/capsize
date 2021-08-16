4f9a876acb4a605266902c0fbd907760cfbc77a8
Michael Taranto <mtaranto@seek.com.au>
2021-08-16 15:32:40 +1000

    refactor: Types

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -6,45 +6,43 @@ export interface FontMetrics {
   capHeight: number;
 }
 
-type NotCSSValues = {
-  lineHeight?: never;
-  capHeightTrim?: never;
-  baselineTrim?: never;
-};
-
-type CapHeightWithLeading = {
-  capHeight: number;
-  leading?: number;
-  fontMetrics: FontMetrics;
-} & NotCSSValues;
-
-type CapHeightWithLineGap = {
-  capHeight: number;
-  lineGap: number;
-  fontMetrics: FontMetrics;
-} & NotCSSValues;
-
-type FontSizeWithLeading = {
-  fontSize: number;
-  leading?: number;
-  fontMetrics: FontMetrics;
-} & NotCSSValues;
-
-type FontSizeWithLineGap = {
-  fontSize: number;
-  lineGap: number;
-  fontMetrics: FontMetrics;
-} & NotCSSValues;
-
-export type CapsizeOptions =
-  | CapHeightWithLineGap
-  | CapHeightWithLeading
-  | FontSizeWithLineGap
-  | FontSizeWithLeading;
-
 export type CapsizeCSSValues = {
   fontSize: string;
   lineHeight: string;
   capHeightTrim: string;
   baselineTrim: string;
 };
+
+type NotCapsizeCSSValues = {
+  [V in keyof CapsizeCSSValues]?: never;
+};
+
+type CapHeightWithLeading = {
+  capHeight: number;
+  leading?: number;
+  fontMetrics: FontMetrics;
+} & NotCapsizeCSSValues;
+
+type CapHeightWithLineGap = {
+  capHeight: number;
+  lineGap: number;
+  fontMetrics: FontMetrics;
+} & NotCapsizeCSSValues;
+
+type FontSizeWithLeading = {
+  fontSize: number;
+  leading?: number;
+  fontMetrics: FontMetrics;
+} & Omit<NotCapsizeCSSValues, 'fontSize'>;
+
+type FontSizeWithLineGap = {
+  fontSize: number;
+  lineGap: number;
+  fontMetrics: FontMetrics;
+} & Omit<NotCapsizeCSSValues, 'fontSize'>;
+
+export type CapsizeOptions =
+  | CapHeightWithLineGap
+  | CapHeightWithLeading
+  | FontSizeWithLineGap
+  | FontSizeWithLeading;

151e940e442734b87d6531d6b2ece2fa286ba48c
Remus Mate <3297808+mrm007@users.noreply.github.com>
2024-01-15 09:19:07 +1100

    Add missing Babel plugin needed to attach Vanilla Extract file scopes (#157)

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,5 @@ module.exports = {
     '@babel/preset-typescript',
     ['@babel/preset-env', { targets: { node: 16, browsers: 'since 2017-06' } }],
   ],
+  plugins: ['@vanilla-extract/babel-plugin'],
 };

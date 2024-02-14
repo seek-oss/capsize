bc03aea77a7cbcfc91c873bd9c20862a52f0e2e1
Remus Mate <mrm_dev@outlook.com>
2024-02-14 16:21:57 +1100

    clean up unused deps

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-typescript',
-    ['@babel/preset-env', { targets: { node: 16, browsers: 'since 2017-06' } }],
-  ],
-  plugins: ['@vanilla-extract/babel-plugin'],
-};

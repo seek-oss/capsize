193d5ec43b7eaf872574a4920cec69f11937945f
Michael Taranto <michaeltaranto@users.noreply.github.com>
2024-02-16 09:57:59 +1100

    xWidthAvg: Update character frequency weightings data source (#167)

---
+++ packages/metrics/scripts/buildMetrics.ts
@@ -0,0 +1,41 @@
+import { Font, fromFile, fromUrl } from '@capsizecss/unpack';
+
+type FontCategory =
+  | 'serif'
+  | 'sans-serif'
+  | 'monospace'
+  | 'display'
+  | 'handwriting';
+export interface MetricsFont extends Font {
+  category: FontCategory;
+}
+
+interface Options {
+  fontSource: string;
+  sourceType: 'file' | 'url';
+  category: FontCategory;
+  overrides?: Partial<Font>;
+}
+
+const extractor: Record<
+  Options['sourceType'],
+  typeof fromFile | typeof fromUrl
+> = {
+  file: fromFile,
+  url: fromUrl,
+};
+
+export const buildMetrics = async ({
+  fontSource,
+  sourceType,
+  category,
+  overrides = {},
+}: Options): Promise<MetricsFont> => {
+  const metrics = await extractor[sourceType](fontSource);
+
+  return {
+    ...metrics,
+    ...overrides,
+    category,
+  };
+};

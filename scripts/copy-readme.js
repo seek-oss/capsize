a4156b9bf6e92cde656c598b701b504ad6b37ab5
Thomas Jaggi <thomas@responsive.ch>
2025-01-04 08:56:55 +0100

    Rewrite relative links when copying README

--- scripts/copy-readme.js
+++ scripts/copy-readme.js
@@ -1,9 +1,20 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+// Copy README.md from root to packages/core and rewrite relative links
 (async () => {
-  await fs.copyFile(
-    path.join(__dirname, '../README.md'),
-    path.join('packages/core', 'README.md'),
+  const sourceDir = path.join(__dirname, '..');
+  const targetDir = path.join(__dirname, '../packages/core');
+  const relativePath = path.relative(targetDir, sourceDir);
+  const content = await fs.readFile(path.join(sourceDir, 'README.md'), {
+    encoding: 'utf-8',
+  });
+
+  // Assume that everything not starting with # or http(s): is a relative link
+  const transformed = content.replace(
+    /]\((?!(#|https?:))/g,
+    `](${relativePath}/`,
   );
+
+  await fs.writeFile(path.join(targetDir, 'README.md'), transformed);
 })();

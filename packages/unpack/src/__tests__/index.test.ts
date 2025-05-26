b72740f8d0d1c001e5a66af2649b3361766cef79
chyzwar <schizek.marcin@gmail.com>
2025-05-26 23:20:45 +0200

    chore(unpack): use expectedMetrics instead of snapshot test

--- packages/unpack/src/__tests__/index.test.ts
+++ packages/unpack/src/__tests__/index.test.ts
@@ -2,13 +2,34 @@ import { readFile } from 'node:fs/promises';
 import { fromUrl, fromBlob, fromBuffer } from '../index';
 import { join } from 'node:path';
 
+const expectedMetrics = {
+  ascent: 1069,
+  capHeight: 714,
+  descent: -293,
+  familyName: 'Noto Sans',
+  fullName: 'Noto Sans Regular',
+  lineGap: 0,
+  postscriptName: 'NotoSans-Regular',
+  subsets: {
+    latin: {
+      xWidthAvg: 474,
+    },
+    thai: {
+      xWidthAvg: 600,
+    },
+  },
+  unitsPerEm: 1000,
+  xHeight: 536,
+  xWidthAvg: 474,
+};
+
 describe('unpack font metrics', () => {
   describe('fromBuffer', () => {
     it('should return the font metrics', async () => {
       const fontPath = join(__dirname, './__fixtures__/NotoSans-Regular.ttf');
       const font = await readFile(fontPath);
       const metrics = await fromBuffer(font);
-      expect(metrics).toMatchSnapshot();
+      expect(metrics).toEqual(expectedMetrics);
     });
   });
 
@@ -19,7 +40,7 @@ describe('unpack font metrics', () => {
       const fontBlob = new Blob([font]);
 
       const metricsBlob = await fromBlob(fontBlob);
-      expect(metricsBlob).toMatchSnapshot();
+      expect(metricsBlob).toEqual(expectedMetrics);
 
       const metricsFont = await fromBuffer(font);
       expect(metricsFont).toEqual(metricsBlob);
@@ -34,7 +55,7 @@ describe('unpack font metrics', () => {
         'https://github.com/notofonts/notofonts.github.io/raw/refs/heads/main/fonts/NotoSans/full/ttf/NotoSans-Regular.ttf';
 
       const metricsUrl = await fromUrl(sampleFontUrl);
-      expect(metricsUrl).toMatchSnapshot();
+      expect(metricsUrl).toEqual(expectedMetrics);
 
       const metricsFont = await fromBuffer(font);
       expect(metricsFont).toEqual(metricsUrl);

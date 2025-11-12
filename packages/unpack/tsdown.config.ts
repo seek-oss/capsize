f564aa3eac82c5b811396434bf1451391d8f8cb3
Chris Swithinbank <swithinbank@gmail.com>
2025-11-12 01:08:57 +0100

    build: Fix tsdown builds (#241)

--- packages/unpack/tsdown.config.ts
+++ packages/unpack/tsdown.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'tsdown';
-import { baseConfig } from '../../tsdown.base.config';
+import { baseConfig } from '../../tsdown.base.config.ts';
 
 export default defineConfig(baseConfig);

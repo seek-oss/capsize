a5f8cbe2339b0498b9b6e1dcc8e7861e2186ddb8
Adam Skoufis <askoufis@seek.com.au>
2025-11-25 14:13:49 +1100

    Bundle `@capsizecss/vanilla-extract` with filescopes

---
+++ .changeset/ninety-hounds-act.md
@@ -0,0 +1,5 @@
+---
+"@capsizecss/vanilla-extract": patch
+---
+
+Fix `Styles were unable to be assigned to a file` errors caused by missing filescopes
--- packages/vanilla-extract/tsdown.config.ts
+++ packages/vanilla-extract/tsdown.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'tsdown';
 import { baseConfig } from '../../tsdown.base.config.ts';
+import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin';
 
 export default defineConfig({
   ...baseConfig,
   unbundle: true,
+  plugins: [
+    vanillaExtractPlugin({
+      unstable_injectFilescopes: true,
+    }),
+  ],
 });
